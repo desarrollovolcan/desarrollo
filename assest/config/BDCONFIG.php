53640f1cea7ec80c2f1cbf1a7c6efafbf17fa546
erwinislasegura <erwin.2785@gmail.com>
2025-07-04 14:45:02 -0400

    Carga inicial
    Carga inicial repositorio Desarrollo

---
+++ assest/config/BDCONFIG.php
@@ -0,0 +1,38 @@
+<?php
+class BDCONFIG {
+
+    private $HOST;
+    private $USER;
+    private $PASS;
+    private $DBNAME;
+
+    public function __construct()
+    {
+        $this->HOST = "localhost";
+        $this->USER = "sm_control";
+        $this->PASS ="p7APm4.RxciUeyS-" ;
+        $this->DBNAME = "smartberry_control";
+
+    }
+
+    public function __GET($k) {
+        return $this->$k;
+    }
+
+    public function __SET($k, $v) {
+        $this->$k = $v;
+    }
+
+    public function conectar() {
+        try {
+            $link = new PDO("mysql:host={$this->HOST};dbname={$this->DBNAME}", $this->USER, $this->PASS);
+            $link->exec("set names utf8");
+            $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $link;
+        } catch (PDOException $e) {
+            // Maneja el error de conexión aquí, si es necesario
+            echo 'Error de conexión: ' . $e->getMessage();
+            return null;
+        }
+    }
+}
