f1d49ff2ceaecada1dafb1fa0d1015362c82fb4b
AstralDisast <103266730+AstralDisast@users.noreply.github.com>
2025-07-05 02:14:01 -0400

    Boton eliminar intructivo carga

--- assest/config/datosUrLP.php
+++ assest/config/datosUrLP.php
@@ -32,7 +32,14 @@ if (isset($_REQUEST['HABILITARURL'])) {
     $_SESSION["urlO"] = $_REQUEST['URLO'];
     echo "<script type='text/javascript'> location.href ='" . $_REQUEST['URL'] . ".php?op&id=".$id_dato."&a=".$accion_dato."';</script>";
 }
-
+if (isset($_REQUEST['DESHABILITARURL'])) {
+    /*$_SESSION["parametro"] = $_REQUEST['ID'];
+    $_SESSION["parametro1"] = "1";*/
+    $id_dato = $_REQUEST['ID'];
+    $accion_dato = "0";
+    $_SESSION["urlO"] = $_REQUEST['URLO'];
+    echo "<script type='text/javascript'> location.href ='" . $_REQUEST['URL'] . ".php?op&id=".$id_dato."&a=".$accion_dato."';</script>";
+}
 
 if (isset($_REQUEST['VERMOTIVOSRURL'])) {
 
